1337f99f5b238683c6af2ab7cd0ee1564877b150
aminskey <aminskey@github.com>
2024-09-05 13:39:10 +0200

    Adding button functionality 1/2 - forgot to upload button.py lol

--- button.py
+++ button.py
@@ -1,25 +1,36 @@
 # More to come
 import pygame
+import json
 
 from pygame.locals import *
 from variables import *
 
 class Button(pygame.sprite.Sprite):
-    def __init__(self, image, size, msgObj):
+    def __init__(self, image, size):
         super().__init__()
         tmp = pygame.image.load(image)
+        #self.__data = json.load(jdata)
         self.base_image = pygame.transform.scale(tmp, size)
-        self.h_image = pygame.transform.scale(pygame.image.load("assets/buttons/buttonDemo2.png"), size)
+        self.h_image = pygame.transform.scale(pygame.image.load("assets/buttons/Marine life button 2.png"), size)
         self.image = self.base_image.copy()
         self.rect = self.image.get_rect()
-        self.__msgObj = msgObj
+        self.__activated = False
 
-        self.__msgObj.rect.center = (self.image.get_width()//2, self.image.get_height()//2)
-        self.base_image.blit(self.__msgObj.image, self.__msgObj.rect)
-        self.h_image.blit(self.__msgObj.image, self.__msgObj.rect)
+    def addText(self, msgObj, pos):
+        msgObj.rect.center = pos
+        self.base_image.blit(msgObj.image, msgObj.rect)
+        self.h_image.blit(msgObj.image, msgObj.rect)
 
     def update(self, *args, **kwargs):
         if isInBounds(pygame.mouse.get_pos(), 0, self.rect.topleft, self.rect.bottomright):
             self.image = self.h_image.copy()
+            if pygame.mouse.get_pressed()[0]:
+                self.__activated = True
+            else:
+                self.__activated = False
         else:
             self.image = self.base_image.copy()
+
+    @property
+    def activated(self):
+        return self.__activated
