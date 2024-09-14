f77f358ceabd70a2b4a0a6cc60ae2d1040b0d1f9
aminskey <aminskey@github.com>
2024-09-14 18:06:16 +0200

    Modifying gui files and adding gameover music + CREDITS.md

---
+++ CREDITS.md
@@ -0,0 +1,25 @@
+# Marine Life
+#### Developers
+Max Aaron Bühler <br>
+Syed Muhammad Amin
+
+#### UI Designers and Artists
+Johan Vejgaard Hedegaard <br>
+Syed Muhammad Amin
+
+#### Project Coordinators
+Max Aaron Bühler <br>
+Syed Muhammad Amin <br>
+Johan Vejgaard Hedegaard
+
+#### Music
+In Game Music: Campfire by Sulyya <br>
+Main Menu Music: Ocean Wave by Ambient Boy <br>
+Game Over: Sonic 3 Game Over by SEGA Sound Team <br>
+Unused: 
+Credits: Undecided.
+
+#### Documentation
+Max Aaron Bühler <br>
+Syed Muhammad Amin <br>
+Johan Vejgaard Hedegaard
--- guiInfoPage.py
+++ guiInfoPage.py
@@ -7,7 +7,6 @@ from text import Text
 from vector import Vector
 
 def main(screen):
-
     for b in bubbleGrp.sprites():
         b.kill()
 
@@ -65,68 +64,3 @@ def main(screen):
 
         pygame.display.update()
         clock.tick(FPS)
-
-def parseMD(file, margin, screen, **kwargs):
-    tmpGrp = pygame.sprite.Group()
-
-    basicFont = pygame.font.Font("assets/fonts/pixelart.ttf", 25)
-    codeFont = pygame.font.Font("assets/fonts/Flexi_IBM_VGA_True.ttf", 25)
-    titleFont = pygame.font.Font("assets/fonts/pixelart.ttf", 75)
-    h2Font = pygame.font.Font("assets/fonts/pixelart.ttf", 50)
-    h3Font = pygame.font.Font("assets/fonts/pixelart.ttf", 30)
-    h4Font = pygame.font.Font("assets/fonts/pixelart.ttf", 27)
-
-    with open(file, "rb") as f:
-        buff = f.read().decode().split('\n')
-        buff = " ".join(buff).split(' ')
-        f.close()
-
-    print(buff)
-
-    pos = Vector(margin, 0)
-    script = basicFont
-    for word in buff:
-        if "```" in word:
-            if script is not codeFont:
-                script = codeFont
-                margin = 25
-
-            elif script is codeFont:
-                script = basicFont
-                margin = 10
-            pos = Vector(margin, pos.y)
-            continue
-        elif "#" in word and script is not codeFont:
-            script = titleFont
-            if word.count("#") == 2:
-                script = h2Font
-            elif word.count("#") == 3:
-                script = h3Font
-                script.underline = True
-            elif word.count("#") == 4:
-                script = h4Font
-            continue
-
-        if word == "":
-            word = " "
-
-        tmp = Text(word.strip()+" ", script, **kwargs)
-        tmp.rect.topleft = pos.tuple
-        tmpGrp.add(tmp)
-
-        # Prepping position for next word.
-
-        if len(word) > 0:
-            if word[-1] == '\r' or pos.x > screen.get_width() * 3//4:
-                print(pos.y)
-                pos.y += script.size(word)[1]
-                pos.x = margin
-
-                if script is not codeFont:
-                    script = basicFont
-                script.underline = False
-            else:
-                pos.x = tmpGrp.sprites()[-1].rect.topright[0]
-                pos.y = tmpGrp.sprites()[-1].rect.topright[1]
-
-    return tmpGrp
