df3008d78e343131881d0303260406150a3cffc3
aminskey <aminskey@github.com>
2024-09-26 14:15:13 +0200

    Fixed gameover

--- guiGameOver.py
+++ guiGameOver.py
@@ -4,16 +4,20 @@ from text import Text
 from pygame.locals import *
 from variables import WHITE, BLACK, addVec, clock, OCEANSHADOW
 from vector import Vector
+from time import time
 
 def gameOver(screen):
     pygame.mixer.music.load("assets/music/InGame/gameOver.ogg")
 
     font = pygame.font.Font("assets/fonts/pixelart.ttf", 65)
+    pfont = pygame.font.Font("assets/fonts/pixelart.ttf", 25)
 
     title = Text("Game Over", font, WHITE, shadow=OCEANSHADOW)
+    subtitle = Text("Press anything to continue..", pfont, WHITE, shadow=OCEANSHADOW)
     bg = screen.copy()
     shade = pygame.Surface(screen.get_size())
     shade.fill(BLACK)
+    bg.set_alpha(50)
     shade.set_alpha(50)
 
 
@@ -24,6 +28,7 @@ def gameOver(screen):
 
     running = True
     pygame.mixer.music.play()
+    startTime = time()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -42,10 +47,14 @@ def gameOver(screen):
                 acc = Vector(0, 0)
 
         addVec(title.rect, vel)
+        subtitle.rect.midtop = title.rect.midbottom
 
         screen.blit(bg, (0, 0))
         screen.blit(shade, (0, 0))
         screen.blit(title.image, title.rect)
+        currTime = time()
+        if (currTime - startTime) > 7:
+            screen.blit(subtitle.image, subtitle.rect)
 
         pygame.display.update()
         clock.tick(30)
